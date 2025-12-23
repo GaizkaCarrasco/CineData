3f3c52d6be96b91ec904ae5a003e5301e92392ce
GaizkaC <oskargaizka@gmail.com>
2025-12-23 16:14:18 +0100

    Se descargan las pelis de la api externa y se guardan en la base de datos con un endpoint

--- movieService/routes/movies.js
+++ movieService/routes/movies.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 const db = require("../db");
 
 router.post("/", async (req, res) => {
@@ -15,6 +16,43 @@ router.post("/", async (req, res) => {
     res.status(201).json({ message: "Movie saved" });
   } catch (err) {
     res.status(500).json({ error: err.message });
+  }
+});
+
+router.post("/import", async (req, res) => {
+  try {
+    //Llamada a la API externa
+    const response = await axios.get(
+      "https://devsapihub.com/api-movies"
+    );
+
+    const movies = response.data;
+
+    //Guardar cada película
+    for (const movie of movies) {
+      await db.query(
+        `INSERT IGNORE INTO movies 
+        (external_id, title, description, year, image_url, genre, stars)
+        VALUES (?, ?, ?, ?, ?, ?, ?)`,
+        [
+          movie.id,
+          movie.title,
+          movie.description,
+          movie.year,
+          movie.image_url,
+          movie.genre,
+          movie.stars,
+        ]
+      );
+    }
+
+    res.json({
+      message: "Películas importadas correctamente",
+      total: movies.length,
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
